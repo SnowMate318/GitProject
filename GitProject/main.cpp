8f790465a28d127a0feaafea230836c050e2c7c9
taehyun <xogus5070318@gmail.com>
2025-08-20 16:16:19 +0900

    메인함수 수정 완료

--- GitProject/main.cpp
+++ GitProject/main.cpp
@@ -7,26 +7,16 @@ int main() {
 
 	int cmd = -1;
 
-	//b1.info_of_book();
+	UserUI user = UserUI();
+	Admin admin = Admin();
 
-// 	Admin admin;
-// 	admin.printMenu();
-
-	while (cmd != 1 && cmd != 2) {
-		std::cout << "모드 선택 1. 사용자    2. 관리자      " << std::endl;
-		cin >> cmd;
-	}
-	if (cmd == 1) {
-		UserUI user = UserUI();
-		while (1) {
-			user.printMenu();
-		}
-		return 0;
-	}
-	while (1) {
-		Admin admin = Admin();
+	while (true) {
+		user.printMenu();
 		admin.printMenu();
 	}
+	
+	
+	
 	//함수 추가
 	return 0;
 }

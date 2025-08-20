59a8df43d951ebf34e090e55aef218625233c44b
DESKTOP-Q4V5NDH\DSO18 <hojoonx@gmail.com>
2025-08-20 10:58:48 +0900

    책 생성 함수 구현

--- GitProject/LibraryClass.cpp
+++ GitProject/LibraryClass.cpp
@@ -1,1 +1,37 @@
 #include "LibraryClass.h"
+#include <map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+static map<int, Book> bookList;
+static map<int, bool> bookRent;
+
+void LibraryClass::createBook(Book* book)
+{
+	bookList[book->getId()] = *book;
+	bookRent[book->getId()] = false;
+}
+
+//void LibraryClass::deleteBook(int bookId)
+//{
+//
+//}
+//
+//void LibraryClass::findBook(int bookId)
+//{
+//	bool bookExists = bookList.find(bookId);
+//
+//	if (bookExists) {
+//		cout << "책이 있습니다. " << endl;
+//	}
+//}
+//void LibraryClass::rentBook(int bookId)
+//{
+//
+//}
+//void LibraryClass::returnBook(int bookId)
+//{
+//
+//}
--- GitProject/LibraryClass.h
+++ GitProject/LibraryClass.h
@@ -4,10 +4,9 @@ class Book;
 class LibraryClass
 {
 public:
-	void createBook(Book book);
+	void createBook(Book* book);
 	void deleteBook(int bookId);
 	void findBook(int bookId);
 	void rentBook(int bookId);
 	void returnBook(int bookId);
 };
-

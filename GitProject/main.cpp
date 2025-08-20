381dfb80f4acede676e190b9c8971318c6dd5f86
taehyun <xogus5070318@gmail.com>
2025-08-20 15:33:20 +0900

    메인함수 구현

--- GitProject/main.cpp
+++ GitProject/main.cpp
@@ -1,10 +1,26 @@
 #include "book.h"
 #include <iostream>
 #include "admin.h"
+#include "userUI.h"
 int main() {
 
-	std::cout << "Hello World" << std::endl;
-	
+	int cmd = -1;
+
+	while (cmd != 1 && cmd != 2) {
+		std::cout << "모드 선택 1. 사용자    2. 관리자      " << std::endl;
+		cin >> cmd;
+	}
+	if (cmd == 1) {
+		UserUI user = UserUI();
+		while (1) {
+			user.printMenu();
+		}
+		return 0;
+	}
+	while (1) {
+		Admin admin = Admin();
+		admin.printMenu();
+	}
 	//함수 추가
 	return 0;
 }
